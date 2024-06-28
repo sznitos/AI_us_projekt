8721566cb8e8e4d0069188e9ac991a502db469fa
sznitos <sznitos@gmail.com>
2024-06-28 09:33:09 +0200

    uszeregowano

--- routing.php
+++ routing.php
@@ -11,24 +11,19 @@ Utils::addRoute('startPage', 'StartPageCtrl');
 Utils::addRoute('login', 'LoginCtrl');
 Utils::addRoute('logout', 'LoginCtrl');
 
-//Utils::addRoute('library', 'LibraryCtrl');
+
 Utils::addRoute('library', 'LibraryCtrl', ["user", "admin"]);
-//Utils::addRoute('returnBook', 'LibraryCtrl', ["user", "admin"]);
+Utils::addRoute('borrowBook', 'LibraryCtrl',["user", "admin"]);
+Utils::addRoute('returnBook', 'LibraryCtrl',["user", "admin"]);
 
-Utils::addRoute('profile', 'ProfileCtrl', ["user", "admin"]);
-
-//Utils::addRoute('manage', 'ManageCtrl', ["admin"]);
 Utils::addRoute('bookNew', 'ManageEditCtrl', ["admin"]);
 Utils::addRoute('bookEdit', 'ManageEditCtrl', ["admin"]);
 Utils::addRoute('bookDelete', 'ManageEditCtrl', ["admin"]);
 Utils::addRoute('bookSave', 'ManageEditCtrl', ["admin"]);
 
-Utils::addRoute('borrowBook', 'LibraryCtrl',["user", "admin"]);
-Utils::addRoute('returnBook', 'LibraryCtrl',["user", "admin"]);
+
 Utils::addRoute('profileReturn', 'ProfileCtrl', ["user", "admin"]);
+Utils::addRoute('profile', 'ProfileCtrl', ["user", "admin"]);
 
 
-//Utils::addRoute('personNew',     'PersonEditCtrl',	['user','admin']);
-//Utils::addRoute('personEdit',    'PersonEditCtrl',	['user','admin']);
-//Utils::addRoute('personSave',    'PersonEditCtrl',	['user','admin']);
-//Utils::addRoute('personDelete',  'PersonEditCtrl',	['admin']);
+
